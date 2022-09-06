9122fd34e5a369595f4b1a42356702dc0381f715
GEAPS <listenonepiece@gmail.com>
2022-09-06 23:07:39 +0100

    add new env entrance

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,3 +2,4 @@ from envs.robobin import RoboBinEnv
 from envs.dmc import DmcEnv
 from envs.kitchen import KitchenEnv
 from envs.multiplexing import MultiplexedEnv, PadActions
+from envs.toy_maze import PointMaze2D
