7e2e7adc930498243e9e5ecf31d8013f90dea565
GEAPS <listenonepiece@gmail.com>
2022-09-13 13:42:03 +0100

    add ant maze env

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,3 +3,4 @@ from envs.dmc import DmcEnv
 from envs.kitchen import KitchenEnv
 from envs.multiplexing import MultiplexedEnv, PadActions
 from envs.toy_maze import PointMaze2D
+from envs.ant_maze import AntMaze
